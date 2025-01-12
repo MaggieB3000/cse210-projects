178d2a2bd8ba605a41eb43ffaf80d43455906a65
MaggieB3000 <bin20017@byui.edu>
2025-01-11 21:41:20 -0700

    W01 Prove: Developer—C# Programming Parts 1–2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,51 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.WriteLine("What is your grade percentage? ");
+        string x = Console.ReadLine();
+        int grade_percent = int.Parse(x);
+
+        string letter = "";
+
+        if (grade_percent >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade_percent >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade_percent >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade_percent >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine ($"Your grade is: {letter}");
+
+        if (grade_percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+
+
+
     }
 }

f2e47404120576fd83d522056f20c84ae31b2248
MaggieB3000 <bin20017@byui.edu>
2025-01-16 21:35:01 -0700

    Implemented functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,47 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string number_string = Console.ReadLine();
+        int number = Convert.ToInt32(number_string);
+        
+        return number;
+    }
+
+    static int SquareNumber (int number)
+    {
+        return number * number;
+    }
+
+    static void DisplayResult(string name, int squared_number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squared_number}.");
+    }
+
+
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string user_name = PromptUserName();
+        int user_number = PromptUserNumber();
+        int squared_number = SquareNumber(user_number);
+        DisplayResult (user_name, squared_number);
     }
 }

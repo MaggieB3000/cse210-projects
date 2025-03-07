87665c11899f4665e0387cb136af0efb744652e9
MaggieB3000 <bin20017@byui.edu>
2025-03-06 23:19:14 -0700

    Week09 Prove: shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square shape1 = new Square("Red", 3);
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle("Blue", 2, 4);
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Yellow", 5);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }
 }

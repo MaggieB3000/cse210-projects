eaa02982f82e2bcaa26c2edef521d9d1dba40563
maggieb3000 <bin20017@byui.edu>
2025-04-03 22:26:36 -0600

    truncated numbers in foundation 4

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -26,6 +26,6 @@ abstract class Activity
 
     public virtual string GetSummary()
     {
-        return $"{_date} {_type} ({_duration} min): Distance {GetDistance()} km, Speed: {GetSpeed()} kph, Pace: {GetPace()} min per km";
+        return $"{_date} {_type} ({_duration} min): Distance {GetDistance(),3:F2} km, Speed: {GetSpeed(),3:F2} kph, Pace: {GetPace(),3:F2} min per km";
     }
 }
